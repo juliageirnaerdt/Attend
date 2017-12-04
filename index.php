8e11709d8ea9abf6d99537dc64b6ce33e387e3d1
Julia Geirnaerdt <julia@geirnaerdt.nl>
2017-12-04 12:03:03 +0100

    DEMO
    Eerste puntjes

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<?php $page_title ="Attend"; ?>
+	<?php $fb_page_id = "339755068127"; ?>
+
+	<div class="page-header">
+
+		<h1><?php echo $page_title; ?></h1>
+
+	</div>
+
+	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+</head>
+<body>
+
+
+	<div class="container">
+
+		<!-- events will be here -->
+
+	</div>
+
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+</body>
+</html>
